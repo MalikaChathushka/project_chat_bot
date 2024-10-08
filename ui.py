b764979bf1832dc99fa6598d8f8d46b2239b99f6
MalikaChathushka <malika.cdabare@gmail.com>
2024-10-08 13:55:57 +0300

    Set up basic user interface

---
+++ ui.py
@@ -0,0 +1,8 @@
+def start_chat():
+    print("Welcome to the Chatbot!")
+    while True:
+        user_input = input("You: ")
+        if user_input.lower() == "exit":
+            print("Goodbye!")
+            break
+        yield user_input
