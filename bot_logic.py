943a67e4e7b3463b700dd4f0f2fbae78785f1e33
MalikaChathushka <malika.cdabare@gmail.com>
2024-10-08 14:00:25 +0300

    Added chatbot logic file

---
+++ bot_logic.py
@@ -0,0 +1,5 @@
+# This file will contain the logic for handling inputs and generating responses
+from response_patterns import get_response
+
+def chatbot(user_input):
+    return get_response(user_input)
